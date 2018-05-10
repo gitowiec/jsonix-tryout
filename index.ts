d3e749606ddb8e5e6fd5e206ebdde148a93dc2ca
Marek Zielonkowski <apsikz@gmail.com>
2018-05-11 00:13:41 +0200

    pretty print

--- index.ts
+++ index.ts
@@ -5,9 +5,11 @@ const JPK = require('./mappings/JPK').JPK;
 // console.log(JPK);
 // import {} from './xmlns/xml-primitives';
 var format = require('xml-formatter');
+
 // import * as KodyCECHKRAJOW from './xmlns/http/crd.gov.pl/xml/schematy/dziedzinowe/mf/2013/05/23/eD/KodyCECHKRAJOW';
 // import * as DefinicjeTypy from './xmlns/http/crd.gov.pl/xml/schematy/dziedzinowe/mf/2016/01/25/eD/DefinicjeTypy';
 // import {JPKType} from './xmlns/http/jpk.mf.gov.pl/wzor/2016/03/09/03095';
+
 // JPKTypy.
 /**
  *
@@ -26,60 +28,69 @@ var format = require('xml-formatter');
  * java --add-modules java.xml.bind -jar node_modules/jsonix/lib/jsonix-schema-compiler-full.jar -d mappings -p JPK Schemat_JPK_FA\(1\)_v1-0.xsd -b bindings.xjb
  *
  */
+
+
+
+
+
+
 const value = {
     naglowek: {},
     podmiot1: {
         identyfikatorPodmiotu: {
-            nip: "3213213213",
+            nip       : "3213213213",
             pelnaNazwa: "dsadas adas Z dsadsa dsadsad",
-            regon: "32123213333"
+            regon     : "32123213333"
         },
-        adresPodmiotu: {
+        adresPodmiotu        : {
             kodKraju: 'PL'
         }
     },
-    faktura: [
+    faktura : [
         {
-            "p1": "2018-01-12",
-            "p2A": "FV SP / 198 / 01 / 2018",
-            "p3A": "rewrwer erwerw-r  rew",
-            "p3B": "dsfdsfgfdh dfsfds f sdfds",
-            "p3C": "zzzzzz",
-            "p3D": "eeeee",
-            "p4B": "1232132133",
-            "p5B": "1232132133",
-            "p6": "2018-01-10",
-            "p131": "5316.00",
-            "p141": "1222.68",
-            "p132": "0.00",
-            "p142": "0.00",
-            "p133": "0.00",
-            "p143": "0.00",
-            "p135": "0.00",
-            "p145": "0.00",
-            "p136": "0.00",
-            "p137": "0.00",
-            "p15": "6538.68",
-            "p16": "false",
-            "p17": "false",
-            "p18": "false",
-            "p19": "false",
-            "p20": "false",
-            "p21": "false",
-            "p23": "false",
-            "p106E2": "false",
-            "p106E3": "false",
+            "p1"          : "2018-01-12",
+            "p2A"         : "FV SP / 198 / 01 / 2018",
+            "p3A"         : "rewrwer erwerw-r  rew",
+            "p3B"         : "dsfdsfgfdh dfsfds f sdfds",
+            "p3C"         : "zzzzzz",
+            "p3D"         : "eeeee",
+            "p4B"         : "1232132133",
+            "p5B"         : "1232132133",
+            "p6"          : "2018-01-10",
+            "p131"        : "5316.00",
+            "p141"        : "1222.68",
+            "p132"        : "0.00",
+            "p142"        : "0.00",
+            "p133"        : "0.00",
+            "p143"        : "0.00",
+            "p135"        : "0.00",
+            "p145"        : "0.00",
+            "p136"        : "0.00",
+            "p137"        : "0.00",
+            "p15"         : "6538.68",
+            "p16"         : "false",
+            "p17"         : "false",
+            "p18"         : "false",
+            "p19"         : "false",
+            "p20"         : "false",
+            "p21"         : "false",
+            "p23"         : "false",
+            "p106E2"      : "false",
+            "p106E3"      : "false",
             "rodzajFaktury": "VAT"
         }
     ]
-};
+}
+
 const context = new Jsonix.Context([JPK], {
     namespacePrefixes: {
         "http://jpk.mf.gov.pl/wzor/2016/03/09/03095/": "",
         "http://crd.gov.pl/xml/schematy/dziedzinowe/mf/2016/01/25/eD/DefinicjeTypy/": "ns2"
     }
 });
+
 const marshaller = context.createMarshaller();
+
 const result = marshaller.marshalString({
     JPK: value
 });
