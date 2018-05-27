c083ada979e42a061e27146faf2700489bcd470b
Marek Zielonkowski <apsikz@gmail.com>
2018-05-27 23:21:11 +0200

    wip

--- index2.js
+++ index2.js
@@ -1,20 +1,58 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class KodFormularza {
-}
-const kod = new KodFormularza();
-const Naglowek = {
-    DataDo: new Date(),
-    DataOd: new Date(),
-    CelZlozenia: 1,
-    WariantFormularza: 1,
-    DataWytworzeniaJPK: new Date(),
-    DomyslnyKodWaluty: "PLN",
-    KodFormularza
-};
-const JPK = {
-    Naglowek
-};
+// const dok: document;
+// dok.JPK.Faktura[0].
 const doc = {
-    JPK
+    JPK: {
+        Naglowek: {
+            CelZlozenia: 1,
+            DataDo: new Date(),
+            DataOd: new Date(),
+            DataWytworzeniaJPK: new Date(),
+            DomyslnyKodWaluty: 'PLN',
+            KodFormularza: {
+                kodSystemowy: '0',
+                wersjaSchemy: '1'
+            },
+            KodUrzedu: '0202',
+            WariantFormularza: 1,
+            _exists: true,
+            _namespace: '',
+        },
+        Faktura: [{ _exists: true }],
+        FakturaCtrl: [{ _exists: true }],
+        FakturaWiersz: [{ _exists: true }],
+        FakturaWierszCtrl: [{ _exists: true }],
+        Podmiot1: { _exists: true },
+        StawkiPodatku: { _exists: true },
+        _namespace: '',
+        _exists: true
+    },
+    _exists: true,
+    _namespace: true
 };
+// class KodFormularza implements TNaglowekKodFormularzaType {
+//     content: "JPK_FA";
+//     kodSystemowy: '';
+//     wersjaSchemy: '';
+//     _exists: true;
+//     _namespace: '';
+//
+// }
+// const kod = new KodFormularza();
+//
+// const Naglowek: TNaglowek = {
+//     DataDo: new Date(),
+//     DataOd: new Date(),
+//     CelZlozenia: 1,
+//     WariantFormularza: 1,
+//     DataWytworzeniaJPK: new Date(),
+//     DomyslnyKodWaluty: "PLN",
+//     KodFormularza
+//
+// }
+// const JPK: JPKType = {
+//     Naglowek
+// }
+//
+//
--- index2.ts
+++ index2.ts
@@ -1,36 +1,71 @@
-import * as JPKNS from './xmlns/http/jpk.mf.gov.pl/wzor/2016/03/09/03095'
-import {document, JPKType, TKodFormularza, TNaglowek} from "./xmlns/http/jpk.mf.gov.pl/wzor/2016/03/09/03095";
-import {TNaglowekKodFormularzaType} from "./xmlns/http/jpk.mf.gov.pl/wzor/2016/03/09/03095";
+import * as JPKNS from './xmlns/http/jpk.mf.gov.pl/wzor/2016/03/09/03095';
+import { document, JPKType, TKodFormularza, TNaglowek } from "./xmlns/http/jpk.mf.gov.pl/wzor/2016/03/09/03095";
+import { TNaglowekKodFormularzaType } from "./xmlns/http/jpk.mf.gov.pl/wzor/2016/03/09/03095";
 
 
-class KodFormularza implements TNaglowekKodFormularzaType {
-    content: "JPK_FA";
-    kodSystemowy: '';
-    wersjaSchemy: '';
-    _exists: true;
-    _namespace: '';
+// const dok: document;
 
-}
-
-const kod = new KodFormularza();
-
-const Naglowek: TNaglowek = {
-    DataDo: new Date(),
-    DataOd: new Date(),
-    CelZlozenia: 1,
-    WariantFormularza: 1,
-    DataWytworzeniaJPK: new Date(),
-    DomyslnyKodWaluty: "PLN",
-    KodFormularza
-
-}
-
-
-const JPK: JPKType = {
-    Naglowek
-}
+// dok.JPK.Faktura[0].
 
 
 const doc: document = {
-    JPK
-}
+    JPK: {
+        Naglowek: {
+            CelZlozenia: 1,
+            DataDo: new Date(),
+            DataOd: new Date(),
+            DataWytworzeniaJPK: new Date(),
+            DomyslnyKodWaluty: 'PLN',
+            KodFormularza: {
+                kodSystemowy: '0',
+                wersjaSchemy: '1'
+            },
+            KodUrzedu: '0202',
+            WariantFormularza: 1,
+            _exists: true,
+            _namespace: '',
+            // constructor: () => doc
+        },
+        Faktura: [{_exists: true}],
+        FakturaCtrl: [{_exists: true}],
+        FakturaWiersz: [{_exists: true}],
+        FakturaWierszCtrl: [{_exists: true}],
+        Podmiot1: {_exists: true},
+        StawkiPodatku: {_exists: true},
+        _namespace: '',
+        _exists: true
+    },
+    _exists: true,
+    _namespace: true
+};
+
+
+// class KodFormularza implements TNaglowekKodFormularzaType {
+//     content: "JPK_FA";
+//     kodSystemowy: '';
+//     wersjaSchemy: '';
+//     _exists: true;
+//     _namespace: '';
+//
+// }
+
+// const kod = new KodFormularza();
+//
+// const Naglowek: TNaglowek = {
+//     DataDo: new Date(),
+//     DataOd: new Date(),
+//     CelZlozenia: 1,
+//     WariantFormularza: 1,
+//     DataWytworzeniaJPK: new Date(),
+//     DomyslnyKodWaluty: "PLN",
+//     KodFormularza
+//
+// }
+
+
+// const JPK: JPKType = {
+//     Naglowek
+// }
+//
+//
+
